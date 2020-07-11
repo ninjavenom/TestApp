1f25ce91b68e6f39615c0fcb6086946dc1775938
shiva <shiva.bghangre@gmail.com>
2020-07-11 19:19:14 +0530

    Initial commit

---
+++ app/src/main/java/com/shiva/testapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shiva.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
